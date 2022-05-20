0da589ca706505035339a0f0c27faaf5159068c8
serik.meiirbek@gmail.com <serik.meiirbek@gmail.com>
2022-05-20 10:24:28 -0400

    Added DemoController.java

--- src/main/java/academy/learnprogramming/BootDemoApplication.java
+++ src/main/java/academy/learnprogramming/BootDemoApplication.java
@@ -1,4 +1,4 @@
-package academy.learnprogramming.bootdemo;
+package academy.learnprogramming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
